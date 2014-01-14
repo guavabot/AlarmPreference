0c82042eecb7a03c161ec4f8e8e7840cca9a4dfe
ivansoriano <ivansoriano@gmail.com>
2014-01-14 16:09:54 +0100

    add sample app

---
+++ sample/src/com/guavabot/alarmpreferencedemo/MainPreferences.java
@@ -0,0 +1,17 @@
+package com.guavabot.alarmpreferencedemo;
+
+import com.guavabot.alarmpreferencedemo.R;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class MainPreferences extends PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.project_preferences);
+    }
+
+}
